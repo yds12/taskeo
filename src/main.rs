1f55cc0d399ad1bd1b94868452023b2e8a5e5b59
yds12 <yds12@protonmail.com>
2023-08-28 19:58:00 +0200

    Remove regex as dep and small fixes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use regex::Regex;
 use std::env;
 use task::Task;
 
@@ -12,21 +11,24 @@ const DEFAULT_PRIO: u8 = 4;
 fn list() {
     let tasks = file::get_tasks();
 
-    if let Ok(mut tasks) = tasks {
-        if tasks.len() == 0 {
-            println!("Your TO-DO list is empty. Great!\n");
-            return;
-        }
+    match tasks {
+        Ok(mut tasks) => {
+            if tasks.len() == 0 {
+                println!("Your TO-DO list is empty. Great!\n");
+                return;
+            }
 
-        tasks.sort();
+            tasks.sort();
 
-        println!("Here is your TO-DO list:\n");
-        for (n, task) in tasks.iter().enumerate() {
-            println!(" {:>3}. {}", n + 1, task);
+            println!("Here is your TO-DO list:\n");
+            for (n, task) in tasks.iter().enumerate() {
+                println!(" {:>3}. {}", n + 1, task);
+            }
+            println!("");
         }
-        println!("");
-    } else {
-        println!("Error listing tasks.");
+        Err(e) => {
+            println!("Error listing tasks: {e}");
+        }
     }
 }
 
@@ -34,8 +36,8 @@ fn list() {
 /// might have a problem bigger than an integer overflow.
 fn delete(id: u8) {
     println!("Deleting task {}...", id);
-    if let Err(_) = file::delete_task(id) {
-        println!("Error deleting task.");
+    if let Err(e) = file::delete_task(id) {
+        println!("Error deleting task: {e}");
     }
 }
 
@@ -43,21 +45,24 @@ fn delete(id: u8) {
 fn add(task: &str, priority: u8) {
     println!("Adding task `{}` with priority {}...", task, priority);
     let task = Task::new(task.to_owned(), priority);
-    if let Err(_) = file::add_task(task) {
-        println!("Error adding task.");
+    if let Err(e) = file::add_task(task) {
+        println!("Error adding task: {e}");
     }
 }
 
+fn is_prio(arg: &str) -> bool {
+    ["%", "%%", "%%%", "%%%%"].contains(&arg)
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = env::args().collect::<Vec<_>>();
 
     match args.len() - 1 {
         0 => list(),
         1 if args[1].parse::<u8>().is_ok() => delete(args[1].parse::<u8>().unwrap()),
         1 => add(&args[1][..], DEFAULT_PRIO),
         _ => {
-            let r = Regex::new(r"(%)+").unwrap();
-            if r.is_match(&args[1]) {
+            if is_prio(&args[1]) {
                 add(&args[2..].join(" "), args[1].len() as u8);
             } else {
                 add(&args[1..].join(" "), DEFAULT_PRIO);
